f890bfb5cadd8c5f7377ee90a2b49fed8d8cdf74
VictorHanert <vict660m@stud.kea.dk>
2025-04-12 16:11:29 +0200

    Added CORS and live updates

--- services/image_service/main.py
+++ services/image_service/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 @app.get("/")
 def index():
-    return {"message": "Service Ready"}
+    return {"message": "Image/Geo Location Service Ready"}
